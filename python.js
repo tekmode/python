19703c23fb942b42df61c4bed32f2ffccaf5fd16
Muhammad Rashid Mukhtar <38366545+tekmode@users.noreply.github.com>
2020-01-21 01:18:06 +0500

    Update python.js

--- python.js
+++ python.js
@@ -3,13 +3,13 @@ const ps = require('python-shell');
 console.log(process.env.PATH);
 let options = {
     mode: 'text',
-    //pythonPath: '/usr/bin/python',
+    //pythonPath: '/usr/bin/python',//only enable it if you have python installed different from default location
     pythonOptions: ['-u'], // get print results in real-time
-    scriptPath: 'e:/node',
-    args: ['value1', 'value2']
+    scriptPath: 'e:/node',//Path to your python script
+    args: ['value1', 'value2']//If you want to add some variable that can be accessed in Python script by system.value1 etc
 };
 
-ps.PythonShell.run('myp.py', options, function(err, results) {
+ps.PythonShell.run('myp.py', options, function(err, results) {/running your script file actually
     if (err) throw err;
     console.log('finished');
     console.log(results);
